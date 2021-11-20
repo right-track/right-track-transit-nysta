8edc19fd2d3864a13dd716a3a64130879b8bab15
David Waring <djw64@cornell.edu>
2021-11-19 22:13:30 -0500

    Update Feed for changes to right-track-transit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 const path = require('path');
 const RightTrackTransitAgency = require('right-track-transit');
 const feed = require('./feed.js');
-const props = require('../agency.json');
-const propsDir = path.dirname(require.resolve('../agency.json'));
+
+const moduleDirectory = __dirname + "/../";
 
 /**
  * `RightTrackTransitAgency` implementation for the
@@ -22,61 +22,19 @@ class NYSTA extends RightTrackTransitAgency {
    * Create a `RightTrackTransitAgency` for NYSTA
    */
   constructor() {
-    super(propsDir);
+    super(path.normalize(moduleDirectory));
   }
 
   /**
    * Load the Transit Feed for NYSTA
    * @param {function} callback Callback function
-   * @param {Error} callback.error Transit Feed Error. The Error's message will be a pipe (|) separated
+   * @param {Error} [callback.error] Transit Feed Error. The Error's message will be a pipe (|) separated
    * string in the format of: Error Code|Error Type|Error Message that will be parsed out by the Right
    * Track API Server into a more specific error Response.
    * @param {TransitFeed} [callback.feed] The built Transit Feed for NYSTA
    */
   loadFeed(callback) {
-    feed(callback);
-  }
-
-  /**
-   * Get the local absolute path to the icon for the specified division
-   * @param {string} division Transit Division Code
-   * @returns {string|undefined}
-   */
-  getDivisionIconPath(division) {
-    for ( let i = 0; i < props.divisions.length; i++ ) {
-      if ( props.divisions[i].code === division ) {
-        return path.normalize(propsDir + '/' + props.divisions[i].icon);
-      }
-    }
-    return undefined;
-  }
-
-  /**
-   * Get the background color for the specified line
-   * @param {string} line Transit Line Code
-   * @returns {string}
-   */
-  getLineBackgroundColor(line) {
-    for ( let j = 0; j < props.lines.length; j++ ) {
-      if ( props.lines[j].code === line ) {
-        return props.lines[j].backgroundColor;
-      }
-    }
-    return props.lines[0].backgroundColor;
-  }
-
-  /**
-   * Get the text color for the specified line
-   * @param {string} line Transit Line Code
-   * @returns {string}
-   */
-  getLineTextColor(line) {
-    for ( let j = 0; j < props.lines.length; j++ ) {
-      if ( props.lines[j].code === line ) {
-        return props.lines[j].textColor;
-      }
-    }
-    return props.lines[0].textColor;
+    feed(this.config, callback);
   }
 
 }
